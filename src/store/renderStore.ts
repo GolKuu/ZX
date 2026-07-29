aaf59271937e67632d8d7d4abd3ebc4a72cee6c4
GolKuu <golkuu.goat@gmail.com>
2026-07-29 14:53:08 +0500

    201

--- src/store/renderStore.ts
+++ src/store/renderStore.ts
@@ -7,17 +7,22 @@ import type {
 type RenderState = {
   effectsEnabled: boolean;
   impactVersion: number;
+  xrayFighterId: 'p1' | 'p2' | null;
+  xrayVersion: number;
   zoroAction: ZoroActionId;
   zoroActionVersion: number;
   zoroStance: ZoroStance;
   playZoroAction: (action: ZoroActionId) => void;
   toggleEffects: () => void;
   triggerImpact: () => void;
+  triggerXray: (fighterId: 'p1' | 'p2') => void;
 };
 
 export const useRenderStore = create<RenderState>((set) => ({
   effectsEnabled: true,
   impactVersion: 0,
+  xrayFighterId: null,
+  xrayVersion: 0,
   zoroAction: 'lightPunch',
   zoroActionVersion: 0,
   zoroStance: 'three',
@@ -32,4 +37,9 @@ export const useRenderStore = create<RenderState>((set) => ({
     })),
   toggleEffects: () => set((state) => ({ effectsEnabled: !state.effectsEnabled })),
   triggerImpact: () => set((state) => ({ impactVersion: state.impactVersion + 1 })),
+  triggerXray: (xrayFighterId) =>
+    set((state) => ({
+      xrayFighterId,
+      xrayVersion: state.xrayVersion + 1,
+    })),
 }));
