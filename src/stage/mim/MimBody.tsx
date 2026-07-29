1e5b7719118e65614cfc8250a9a376163c72c3f1
GolKuu <golkuu.goat@gmail.com>
2026-07-29 21:51:32 +0500

    239

--- src/stage/mim/MimBody.tsx
+++ src/stage/mim/MimBody.tsx
@@ -2,6 +2,7 @@
 import type { Material } from 'three';
 import { FighterPart } from '../FighterPart';
 import type { MimMaterials } from './mimMaterials';
+import { MimProps } from './MimProps';
 import type { MimResources } from './mimResources';
 import type { MimRigRefs } from './mimRig';
 
@@ -137,44 +138,5 @@ export function MimBody({
         resources={resources}
       />
     </group>
-  );
-}
-
-function MimProps({
-  materials,
-  refs,
-  resources,
-}: Omit<MimBodyProps, 'outline'>) {
-  return (
-    <>
-      <group ref={refs.cursor}>
-        <mesh geometry={resources.cursor} material={materials.cursor} />
-      </group>
-      <group ref={refs.banana}>
-        <mesh geometry={resources.banana} material={materials.yellow} />
-        <mesh
-          geometry={resources.bananaTip}
-          material={materials.purple}
-          position={[0.22, 0.03, 0]}
-          rotation={[0, 0, -0.7]}
-        />
-      </group>
-      <group ref={refs.chair}>
-        <mesh geometry={resources.chairSeat} material={materials.purple} />
-        <mesh
-          geometry={resources.chairBack}
-          material={materials.hoodie}
-          position={[0, 0.34, -0.18]}
-        />
-        <mesh
-          geometry={resources.chairPost}
-          material={materials.yellow}
-          position={[0, -0.27, 0]}
-        />
-      </group>
-      <group ref={refs.snap}>
-        <mesh geometry={resources.snapRing} material={materials.snap} />
-      </group>
-    </>
   );
 }
