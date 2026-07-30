a38be2b28cded1fb49d4e0b13b7609dae3fef9e1
GolKuu <golkuu.goat@gmail.com>
2026-07-30 07:06:44 +0500

    261

--- src/aang/animation/effects/types.ts
+++
@@ -1,8 +0,0 @@
-import type { EffectMotion } from '../../types';
-
-export interface EffectAnimation {
-  readonly selector: string;
-  readonly keyframes: readonly Keyframe[];
-}
-
-export type EffectLibrary = Partial<Record<EffectMotion, EffectAnimation>>;
